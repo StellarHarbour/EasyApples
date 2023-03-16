0bf5c537a722ee207054c86f9b617c10bab22151
segoi <segoitch@outlook.com>
2023-03-16 09:57:59 +0200

    Enum comparison fixes

--- src/main/java/com/segoitch/easyapples/EasyApples.java
+++ src/main/java/com/segoitch/easyapples/EasyApples.java
@@ -11,14 +11,13 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
 import java.util.Random;
 
 public final class EasyApples extends JavaPlugin  implements Listener{
     Random random = new Random();
 
     ItemStack apple = new ItemStack(Material.APPLE,1 );
-    ItemStack stick = new ItemStack(Material.STICK, random.nextInt(1,2));
+    ItemStack stick = new ItemStack(Material.STICK, random.nextInt(1,3));
     double appleProcChance = 0.05;
     double stickProcChance = 0.20;
     int currentTries = 0;
@@ -32,23 +31,23 @@ public final class EasyApples extends JavaPlugin  implements Listener{
     @EventHandler
     public void onLeavesRightClick(PlayerInteractEvent event) {
         // Check to fire event only one time (because basically it fires once for each hand)
-        if(Objects.requireNonNull(event.getHand()).equals(EquipmentSlot.HAND)
-                && event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.AIR)
-                && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+        if(event.getHand() == EquipmentSlot.HAND &&
+                event.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR &&
+                event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             //event.getPlayer().sendMessage("Hand is free"); //Debug
-            if (Objects.requireNonNull(event.getClickedBlock()).getType() == Material.AZALEA_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.ACACIA_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.BIRCH_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.DARK_OAK_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.JUNGLE_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.MANGROVE_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.OAK_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.FLOWERING_AZALEA_LEAVES
-                    || Objects.requireNonNull(event.getClickedBlock()).getType() == Material.SPRUCE_LEAVES) {
+            if (event.getClickedBlock().getType() == Material.AZALEA_LEAVES
+                    || event.getClickedBlock().getType() == Material.ACACIA_LEAVES
+                    || event.getClickedBlock().getType() == Material.BIRCH_LEAVES
+                    || event.getClickedBlock().getType() == Material.DARK_OAK_LEAVES
+                    || event.getClickedBlock().getType() == Material.JUNGLE_LEAVES
+                    || event.getClickedBlock().getType() == Material.MANGROVE_LEAVES
+                    || event.getClickedBlock().getType() == Material.OAK_LEAVES
+                    || event.getClickedBlock().getType() == Material.FLOWERING_AZALEA_LEAVES
+                    || event.getClickedBlock().getType() == Material.SPRUCE_LEAVES) {
                 //event.getPlayer().sendMessage("Leaves found"); //Debug
                 event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.BLOCK_AZALEA_LEAVES_HIT, 0.10F, 1.0F);
                 currentTries++;
-                event.getPlayer().sendMessage("Tries: " + currentTries); //Debug
+                //event.getPlayer().sendMessage("Tries: " + currentTries); //Debug
                 if(Math.random() <= stickProcChance && currentTries < maxTries) {
                     //event.getPlayer().sendMessage("Chance proc"); //Debug
                     //event.getClickedBlock().breakNaturally(); //if you want drop block
